ec3931544a4ea88242c08c7c01a0416bfa83bdeb
pauldes <paul.desigaud@wavestone.com>
2021-12-22 00:19:47 +0100

    mANIPULATE WITH PANDAS

--- nba_magic_stat/magic.py
+++ nba_magic_stat/magic.py
@@ -1,13 +1,33 @@
 import sqlite3
 
+import pandas
+
 from nba_magic_stat import conf, logger
 
 def connect_to_database():
-    con = sqlite3.connect('basketball.sqlite')
-    cur = con.cursor()
-    return cur
+    #con = sqlite3.connect('basketball.sqlite')
+    con = sqlite3.connect('file:data/basketball.sqlite?mode=rw', uri=True)
+    #cur = con.cursor()
+    return con
 
 def generate_stats():
-    cur = connect_to_database()
-    for row in cur.execute('SELECT * FROM Game LIMIT 5')[:5]:
-        print(row)
+    con = connect_to_database()
+    req = """
+    SELECT 
+        name
+    FROM 
+        sqlite_schema
+    WHERE 
+        type ='table' AND 
+        name NOT LIKE 'sqlite_%';
+    """
+    req = 'SELECT * FROM Game ORDER BY GAME_DATE DESC LIMIT 5'
+    #req = "SELECT * FROM sqlite_schema"
+    # logger.debug(f"Running query {req}")
+    # for row in cur.execute(req):
+    #     print(row)
+
+    res = pandas.read_sql_query(req, con)
+    pandas.set_option("display.max_columns", None)
+    print(res)
+    # query
