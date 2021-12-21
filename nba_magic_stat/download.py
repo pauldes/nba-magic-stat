87545e5ab975f245fd3f28e3893e6ce3fcd94d9e
pauldes <paul.desigaud@wavestone.com>
2021-12-21 21:21:40 +0100

    Add black and run

--- nba_magic_stat/download.py
+++ nba_magic_stat/download.py
@@ -1,4 +1,4 @@
-import kaggle 
+import kaggle
 
 from nba_magic_stat import conf, logger
 
@@ -6,5 +6,5 @@ from nba_magic_stat import conf, logger
 def download_data():
     dataset_name = conf.data.kaggle_dataset
     logger.debug(f"Downloading kaggle dataset {dataset_name}...")
-    kaggle.api.dataset_download_files(dataset_name, path="./data/")
-    #kaggle.api.dataset_download_file(dataset, file_name, path=None, force=False, quiet=True):
+    kaggle.api.dataset_download_files(dataset_name, path="./data/", unzip=True)
+    # kaggle.api.dataset_download_file(dataset, file_name, path=None, force=False, quiet=True):
--- nba_magic_stat/magic.py
+++ nba_magic_stat/magic.py
@@ -1,6 +1,13 @@
+import sqlite3
 
 from nba_magic_stat import conf, logger
 
+def connect_to_database():
+    con = sqlite3.connect('basketball.sqlite')
+    cur = con.cursor()
+    return cur
 
 def generate_stats():
-    pass
+    cur = connect_to_database()
+    for row in cur.execute('SELECT * FROM Game LIMIT 5')[:5]:
+        print(row)
